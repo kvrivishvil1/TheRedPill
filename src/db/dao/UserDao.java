825f291ba4c805fb6f1ce2ec87b00fb10836b080
geldo <tgeld15@freeuni.edu.ge>
2017-06-13 00:01:40 +0400

    UserDao style improved :)

--- src/db/dao/UserDao.java
+++ src/db/dao/UserDao.java
@@ -22,35 +22,37 @@ public class UserDao {
 	private static final String password = MyDbInfo.MYSQL_PASSWORD;
 	private static final String server = MyDbInfo.MYSQL_DATABASE_SERVER;
 
-	private Connection connection;
-
 	public UserDao() throws ClassNotFoundException {
 		try {
-			createConnection();
-		} catch (SQLException e) {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void createConnection() throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		connection = DriverManager.getConnection("jdbc:mysql://" + server, account, password);
+	private Connection createConnection() throws ClassNotFoundException, SQLException {
+		Connection connection = DriverManager.getConnection("jdbc:mysql://" + server, account, password);
 		Statement stm = connection.createStatement();
 		stm.executeQuery("USE " + database);
+		return connection;
 	}
 
 	/**
 	 * gets user-name String as parameter and returns Account object
 	 */
 	public Account getAccount(String searchedUser) throws SQLException {
-		String query = "SELECT * FROM " + DbContract.AccountsTable.TABLE_NAME + " WHERE " + DbContract.AccountsTable.COLUMN_NAME_USERNAME + " = ?";
-		PreparedStatement stm = connection.prepareStatement(query);
-		stm.setString(1, searchedUser);
-		ResultSet rs = stm.executeQuery();
-		rs.last();
-		if (rs.getRow() != 0) {
-			return new Account((String) rs.getObject(DbContract.AccountsTable.COLUMN_NAME_USERNAME),
-					(String) rs.getObject(DbContract.AccountsTable.COLUMN_NAME_PASSWORD));
+		try (Connection connection = createConnection()) {
+			String query = generateQueryForAccount();
+			PreparedStatement stm = connection.prepareStatement(query);
+			stm.setString(1, searchedUser);
+			ResultSet rs = stm.executeQuery();
+			rs.last();
+			if (rs.getRow() != 0) {
+				return new Account((String) rs.getObject(DbContract.AccountsTable.COLUMN_NAME_USERNAME),
+						(String) rs.getObject(DbContract.AccountsTable.COLUMN_NAME_PASSWORD));
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
 		return null;
 	}
@@ -61,27 +63,44 @@ public class UserDao {
 	 * @throws SQLException
 	 */
 	public Person getPersonByUserId(int searchedUserId) throws SQLException {
-		String query = "SELECT * FROM " + DbContract.PersonsTable.TABLE_TABLE + " WHERE " + DbContract.PersonsTable.TABLE_TABLE + "."
-				+ DbContract.PersonsTable.COLUMN_NAME_PERSON_ID + " = (SELECT " + DbContract.PersonAccountMapTable.TABLE_NAME + "."
-				+ DbContract.PersonAccountMapTable.COLUMN_NAME_PERSON_ID + " FROM " + DbContract.PersonAccountMapTable.TABLE_NAME + " WHERE "
-				+ DbContract.PersonAccountMapTable.TABLE_NAME + "." + DbContract.PersonAccountMapTable.COLUMN_NAME_ACCOUNT_ID + " = ?)";
-		PreparedStatement stm = connection.prepareStatement(query);
-		stm.setString(1, Integer.toString(searchedUserId));
-		ResultSet rs = stm.executeQuery();
-		rs.last();
-		if (rs.getRow() != 0) {
-			String gender = (String) rs.getObject(DbContract.PersonsTable.COLUMN_NAME_GENDER);
-			Gender curGender;
-			if (gender.equals(DbContract.PersonsTable.ENUM_MALE)) {
-				curGender = Gender.MALE;
-			} else {
-				curGender = Gender.FEMALE;
+		try (Connection connection = createConnection()) {
+			String query = generateQueryForPerson();
+			PreparedStatement stm = connection.prepareStatement(query);
+			stm.setString(1, Integer.toString(searchedUserId));
+			ResultSet rs = stm.executeQuery();
+			rs.last();
+			if (rs.getRow() != 0) {
+				String gender = (String) rs.getObject(DbContract.PersonsTable.COLUMN_NAME_GENDER);
+				Gender curGender;
+				if (gender.equals(DbContract.PersonsTable.ENUM_MALE)) {
+					curGender = Gender.MALE;
+				} else {
+					curGender = Gender.FEMALE;
+				}
+				return new Person((String) rs.getObject(DbContract.PersonsTable.COLUMN_NAME_FIRSTNAME),
+						(String) rs.getObject(DbContract.PersonsTable.COLUMN_NAME_LASTNAME), curGender,
+						(Date) rs.getObject(DbContract.PersonsTable.COLUMN_NAME_BIRTHDATE));
 			}
-			return new Person((String) rs.getObject(DbContract.PersonsTable.COLUMN_NAME_FIRSTNAME),
-					(String) rs.getObject(DbContract.PersonsTable.COLUMN_NAME_LASTNAME), curGender,
-					(Date) rs.getObject(DbContract.PersonsTable.COLUMN_NAME_BIRTHDATE));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
 		return null;
 
 	}
+
+	private String generateQueryForAccount() {
+		return "SELECT * FROM " + DbContract.AccountsTable.TABLE_NAME + " WHERE "
+				+ DbContract.AccountsTable.COLUMN_NAME_USERNAME + " = ?";
+	}
+
+	private String generateQueryForPerson() {
+		// TODO Auto-generated method stub
+		return "SELECT * FROM " + DbContract.PersonsTable.TABLE_TABLE + " WHERE " + DbContract.PersonsTable.TABLE_TABLE
+				+ "." + DbContract.PersonsTable.COLUMN_NAME_PERSON_ID + " = (SELECT "
+				+ DbContract.PersonAccountMapTable.TABLE_NAME + "."
+				+ DbContract.PersonAccountMapTable.COLUMN_NAME_PERSON_ID + " FROM "
+				+ DbContract.PersonAccountMapTable.TABLE_NAME + " WHERE " + DbContract.PersonAccountMapTable.TABLE_NAME
+				+ "." + DbContract.PersonAccountMapTable.COLUMN_NAME_ACCOUNT_ID + " = ?)";
+	}
+
 }
