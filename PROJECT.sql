a76e9515fc92dd447b43e3908c1a47f45777f125
kvrivishvil1 <skvri15@freeuni.edu.ge>
2017-06-29 05:19:13 +0400

    DB update.  Added Quizes

--- PROJECT.sql
+++ PROJECT.sql
@@ -40,12 +40,11 @@ CREATE TABLE Friends (
         REFERENCES Accounts (account_id)
 );
 
-
 CREATE TABLE Messages (
     message_id INT NOT NULL AUTO_INCREMENT,
     sender_id INT NOT NULL,
     reciever_id INT NOT NULL,
-    message_text VARCHAR(100) NOT NULL,
+    message_text text NOT NULL,
     time_sent DATETIME NOT NULL,
     CONSTRAINT Messages_pk PRIMARY KEY (message_id),
     CONSTRAINT Messages_fk1 FOREIGN KEY (sender_id)
@@ -54,8 +53,6 @@ CREATE TABLE Messages (
         REFERENCES Accounts (account_id)
 );
 
-
-    
 CREATE TABLE Challenges (
     challenge_id INT NOT NULL AUTO_INCREMENT,
     quiz_challenged VARCHAR(50) NOT NULL,
@@ -70,12 +67,68 @@ CREATE TABLE Challenges (
         REFERENCES Accounts (account_id)
 );
 
+CREATE TABLE Quizes (
+    quiz_id INT NOT NULL AUTO_INCREMENT,
+    quiz_name VARCHAR(100) NOT NULL,
+    CONSTRAINT Quizes_pk PRIMARY KEY (quiz_id),
+    CONSTRAINT Quizes_uk UNIQUE (quiz_name)
+);
 
+CREATE TABLE Question_type (
+    question_type_id INT NOT NULL AUTO_INCREMENT,
+    question_type_name VARCHAR(50) NOT NULL,
+    CONSTRAINT Question_type_pk PRIMARY KEY (question_type_id),
+    CONSTRAINT Question_type_uk UNIQUE (question_type_name)
+);
 
+insert into Question_type (queestion_type_name) values
+('Question_Response'),
+('Fill in the Blank'),
+('Multiple Choice'),
+('Picture-Respnse Questions'),
+('Multi-Answer Questions'),
+('Multiple Choice with Multiple Answer');
 
+CREATE TABLE Questions (
+    question_id INT NOT NULL AUTO_INCREMENT,
+    quiz_id INT NOT NULL,
+    question_type_id INT NOT NULL,
+    question TEXT NOT NULL,
+    CONSTRAINT Questions_pk PRIMARY KEY (question_id),
+    CONSTRAINT Question_fk1 FOREIGN KEY (quiz_id)
+        REFERENCES Quizes (quiz_id),
+    CONSTRAINT Question_fk2 FOREIGN KEY (question_type_id)
+        REFERENCES Question_type (question_type_id)
+);
 
+CREATE TABLE Answers (
+    answer_id INT NOT NULL AUTO_INCREMENT,
+    question_id INT NOT NULL,
+    answer_type BOOLEAN NOT NULL,
+    answer TEXT NOT NULL,
+    CONSTRAINT Answers_pk PRIMARY KEY (answer_id),
+    CONSTRAINT Answers_fk1 FOREIGN KEY (question_id)
+        REFERENCES Questions (question_id)
+);
 
+CREATE TABLE Matching_question (
+    matching_question_id INT NOT NULL AUTO_INCREMENT,
+    quiz_id INT NOT NULL,
+    matching_question TEXT NOT NULL,
+    CONSTRAINT Matching_pk PRIMARY KEY (matching_id),
+    CONSTRAINT Matching_question_fk1 FOREIGN KEY (quiz_id)
+        REFERENCES Quiz (quiz_id)
+);
 
+CREATE TABLE Matching_answer (
+    matching_answer_id INT NOT NULL AUTO_INCREMENT,
+    matching_question_id INT NOT NULL,
+    matching_answer TEXT NOT NULL,
+    matching_answers_order TEXT NOT NULL,
+    CONSTRAINT Matching_answer_pk PRIMARY KEY (matching_answer_id),
+    CONSTRAINT Matching_answer_fk1 FOREIGN KEY (matching_question_id)
+        REFERENCES Matching_question (matching_question_id)
+);
 
 
 
