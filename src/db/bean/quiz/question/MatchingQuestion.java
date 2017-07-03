a0e32ca30e916b1adc29d850477b4c1f62628b72
Mtsertsvadze <mtser15@freeuni.edu.ge>
2017-07-03 01:14:14 -0700

    More question types are added

---
+++ src/db/bean/quiz/question/MatchingQuestion.java
@@ -0,0 +1,36 @@
+package db.bean.quiz.question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import db.bean.quiz.Answer;
+
+public class MatchingQuestion extends Question {
+	private String question;
+	private List<Answer> answers;
+	
+	/**
+	 * Constructor for MatchingQuestion object 
+	 * @param question The question's text
+	 * @param answers The question's answers
+	 */
+	public MatchingQuestion(String question, List<Answer> answers){
+		this.question = question;
+		this.answers = answers;
+	}
+
+	@Override
+	public String getQuestion() {
+		return question;
+	}
+
+	@Override
+	public List<Answer> getAnswers() {
+		return new ArrayList<>(answers);
+	}
+
+	@Override
+	public String getQuestionType() {
+		return "Matching";
+	}
+}
